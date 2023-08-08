722f4a0f36f9485783fe2c592899da3012bfc2e4
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2023-08-08 21:00:56 +0200

    Fix coverage and GitHub action warnings (#10)
    * Fix coverage and GitHub action warnings

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -162,7 +162,8 @@ def tests(session: Session) -> None:
     session.install(".")
     session.install("coverage[toml]", "pytest", "pygments")
     try:
-        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+        session.run("coverage", "run", "--parallel", "-m", "pytest", "-o",
+                    "pythonpath=", *session.posargs)
     finally:
         if session.interactive:
             session.notify("coverage", posargs=[])
